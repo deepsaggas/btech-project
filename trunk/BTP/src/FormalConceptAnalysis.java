934974304c2fddba69356adf2d7f54057dd3239e
yash10097@iiitd.ac.in <yash10097@iiitd.ac.in@98cee6df-682a-3255-491e-b594925a992b>
2013-03-12 05:00:18 +0000

    FCA updated with runtime memory details gathering code

--- trunk/BTP/src/FormalConceptAnalysis.java
+++ trunk/BTP/src/FormalConceptAnalysis.java
@@ -17,18 +17,25 @@ public class FormalConceptAnalysis {
 	/**
 	 * @param args
 	 */
+	private static final long MEGABYTE = 1024L * 1024L;
+  
+	public static long bytesToMegabytes(long bytes) {
+	    return bytes / MEGABYTE;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		long startTime = System.currentTimeMillis();
 		FormalConceptAnalysis fca=new FormalConceptAnalysis();
 		Extract1 e1=new Extract1();
 		fca.objects=e1.getFiles();
 		fca.attributes=e1.getIdentifiers();
 		Relation rel = new TreeRelation();
-		System.out.println(fca.objects.size());
-		System.out.println(fca.attributes.size());
+		//System.out.println(fca.objects.size());
+		//System.out.println(fca.attributes.size());
 		for(int i=0;i<fca.objects.size();i++){
 			for(int j=0;j<fca.attributes.get(i).size();j++){
-				rel.add(fca.objects.get(i),fca.attributes.get(i).get(j));
+				rel.add(fca.objects.get(i).getName(),fca.attributes.get(i).get(j));
 			}
 		}
 		Lattice lattice = new HybridLattice(rel);
@@ -36,10 +43,15 @@ public class FormalConceptAnalysis {
 		Iterator<Concept> it = lattice.conceptIterator(Traversal.TOP_OBJSIZE);
 		while(it.hasNext()) {
 			Concept c = it.next();
-			if (c.getObjects().size() >= 10) 
-				System.out.println(c);
-		}
-
+			System.out.println(c.toString());
+			}
+		long stopTime = System.currentTimeMillis();
+	    long elapsedTime = stopTime - startTime;
+	    System.out.println("Elapesed Time: "+elapsedTime);
+	    Runtime runtime = Runtime.getRuntime();
+	    //runtime.gc();
+	    long memory = runtime.totalMemory() - runtime.freeMemory();
+	    //System.out.println("Used memory is bytes: " + memory);
+	    System.out.println("Used memory is megabytes: "+ bytesToMegabytes(memory));
 	}
-
 }
